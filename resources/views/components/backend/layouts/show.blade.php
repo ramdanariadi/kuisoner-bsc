a0eada9cec9249c35ed40a9d7fa66b8aed5a6bd8
ari <ramadanariadi@gmail.com>
2024-12-07 20:04:09 +0800

    check null of resources/views/components/backend/layouts/show.blade.php

--- resources/views/components/backend/layouts/show.blade.php
+++ resources/views/components/backend/layouts/show.blade.php
@@ -24,9 +24,9 @@
             <div class="col">
                 @if ($data != "")
                 <small class="float-end text-muted text-end">
-                    @lang('Updated at'): {{$data->updated_at->diffForHumans()}},
+                    @lang('Updated at'): {{$data->updated_at?->diffForHumans()}},
                     <br class="d-block d-sm-none">
-                    @lang('Created at'): {{$data->created_at->isoFormat('LLLL')}}
+                    @lang('Created at'): {{$data->created_at?->isoFormat('LLLL')}}
                 </small>
                 @endif
             </div>
