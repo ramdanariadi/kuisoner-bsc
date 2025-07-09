d9f027a0a0d5f2142b63a7841c8de82e1de92f74
Ramdan Ariadi <ramadanariadi@gmail.com>
2025-07-09 21:27:58 +0800

    fix: bugfix post viewer_count

--- Modules/Post/Http/Controllers/Backend/PostsController.php
+++ Modules/Post/Http/Controllers/Backend/PostsController.php
@@ -4,6 +4,7 @@ namespace Modules\Post\Http\Controllers\Backend;
 
 use App\Authorizable;
 use App\Http\Controllers\Backend\BackendBaseController;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
@@ -11,6 +12,7 @@ use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 use Modules\Post\Enums\PostStatus;
 use Modules\Post\Enums\PostType;
+use Yajra\DataTables\DataTables;
 
 class PostsController extends BackendBaseController
 {
@@ -146,5 +148,51 @@ class PostsController extends BackendBaseController
         logUserAccess($module_title.' '.$module_action.' | Id: '.$$module_name_singular->id);
 
         return redirect()->route("backend.{$module_name}.show", $$module_name_singular->id);
+    }
+
+    /**
+     * Retrieves the data for the index page of the module.
+     *
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function index_data()
+    {
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'List';
+
+        $page_heading = label_case($module_title);
+        $title = $page_heading.' '.label_case($module_action);
+
+        $$module_name = $module_model::select('id', 'name', 'viewer_count', 'updated_at');
+
+        $data = $$module_name;
+
+        return Datatables::of($$module_name)
+            ->addColumn('action', function ($data) {
+                $module_name = $this->module_name;
+
+                return view('backend.includes.action_column', compact('module_name', 'data'));
+            })
+            ->editColumn('name', '<strong>{{$name}}</strong>')
+            ->editColumn('updated_at', function ($data) {
+                $module_name = $this->module_name;
+
+                $diff = Carbon::now()->diffInHours($data->updated_at);
+
+                if ($diff < 25) {
+                    return $data->updated_at->diffForHumans();
+                }
+
+                return $data->updated_at->isoFormat('llll');
+            })
+            ->rawColumns(['name', 'action'])
+            ->orderColumns(['id'], '-:column $1')
+            ->make(true);
     }
 }
